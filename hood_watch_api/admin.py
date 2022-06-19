4b653bf3ec68ae75b5e89eb82b7730ff4557cf6e
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-06-19 19:41:11 +0300

    models migration

---
+++ hood_watch_api/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Neighborhood)
+admin.site.register(Elder)
+admin.site.register(Resident)
+admin.site.register(Controller)
---
+++ hood_watch_api/models.py
@@ -0,0 +1,50 @@
+from pydoc import describe
+from turtle import title
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+
+# EXtending the default Django User attributes
+class User(AbstractUser):
+    is_controller = models.BooleanField(default=False)
+    is_elder = models.BooleanField(default=False)
+    is_resident = models.BooleanField(default=False)
+
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+    location = models.CharField(max_length=255)
+
+
+
+class Elder(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    email = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    bio = models.TextField()
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE, blank=True)
+
+
+class Resident(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    email = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    bio = models.TextField()
+    neighbourhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+
+
+class Controller(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    email = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    bio = models.TextField()
+    neighbourhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+
+
+
+
+
+
+
