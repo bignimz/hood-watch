0a45adfbbcaafa8c1c09c7b3ab5da4fe2e34cfbe
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-06-27 12:35:54 +0300

    created url paths for different models

--- hood_watch_api/urls.py
+++ hood_watch_api/urls.py
@@ -2,8 +2,10 @@ from django.urls import URLPattern, path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('neighborhood-list/', views.neighborhoodView, name='neighborhood-list'),
-    path('neighborhood-detail/<int:pk>', views.neighborhoodDetail, name='neighborhood-detail'),
+    path('neighborhood-detail/<int:pk>',
+         views.neighborhoodDetail, name='neighborhood-detail'),
     path('create-hood/', views.createNeighborhood, name='create-hood'),
     path('update-hood/<int:pk>', views.updateNeighborhood, name='update-hood'),
     path('delete-hood/<int:pk>', views.deleteNeighborhood, name='delete-hood'),
@@ -13,5 +15,5 @@ urlpatterns = [
     path('create-post/', views.createPost, name='create-post'),
     path('update-post/<int:pk>', views.updatePost, name='update-post'),
     path('delete-post/<int:pk>', views.deletePost, name='delete-post'),
-    
+
 ]
